089eb585eba850799c2fbd68914f194e15f87129
takumus <me@takumus.com>
2016-08-23 17:16:16 +0900

    とりあえずjsonをパースしてみる。

--- tests/node_serial_test/main.js
+++ tests/node_serial_test/main.js
@@ -1,19 +1,25 @@
-var SerialPort = require("serialport")
-var sp = new SerialPort("COM7", {
+const SerialPort = require("serialport");
+
+const sp = new SerialPort("COM7", {
 	baudrate: 9600,
 	parser:SerialPort.parsers.readline("\n")
 });
+
 sp.on('data', (data) => {
-	console.log("[from arduino]:"+data);
+	try{
+		data = JSON.parse(data);
+		console.log(data);
+	}catch(error){
+		console.log("okashii");
+	}
 });
 setInterval(()=>{
 	sp.write("m");
 }, 1000 * 60 * 60);
-
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', (chunk) => {
-    chunk.trim().split('\n').forEach((line) => {
+	chunk.trim().split('\n').forEach((line) => {
 		sp.write(line);
-    });
+	});
 });
