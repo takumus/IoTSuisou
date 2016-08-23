bc17548120dbf177ca8e543e0e406b9170df0ad7
takumus <me@takumus.com>
2016-08-23 19:26:25 +0900

    タスクを整理。

--- tests/node_serial_test/main.js
+++ tests/node_serial_test/main.js
@@ -1,25 +1,62 @@
 const SerialPort = require("serialport");
-
-const sp = new SerialPort("COM7", {
+const port = new SerialPort("/dev/ttyACM0", {
 	baudrate: 9600,
 	parser:SerialPort.parsers.readline("\n")
 });
 
-sp.on('data', (data) => {
-	try{
-		data = JSON.parse(data);
-		console.log(data);
-	}catch(error){
-		console.log("okashii");
+//タスク一覧
+const beginTasks = {
+	//計測開始タスク
+	measure:() => {
+		console.log(now());
+		console.log("水面計測開始");
+		port.write("m");
+	},
+	//ただのSerial送信タスク
+	serial:(args)=>{
+		port.write(args);
 	}
+}
+const endTasks = {
+	//計測終了タスク
+	measure:(data) => {
+		console.log(now());
+		console.log("水面計測完了");
+		console.log("*結果　　 : " + data.result);
+		console.log("*角度　　 : " + data.value);
+		console.log("*角度誤差 : " + data.diff);
+		console.log("*水面距離 : " + (Math.sin(data.value / 180 * Math.PI))*16);
+	}
+}
+
+port.on("data", (data) => {
+	try{
+		const obj = JSON.parse(data);
+		endTasks[obj.type](obj);
+		return;
+	}catch(error){
+	}
+	console.log(data);
 });
-setInterval(()=>{
-	sp.write("m");
+
+setInterval(() => {
+	beginTasks.measure();
 }, 1000 * 60 * 60);
+
+const now = () => {
+	const date = new Date();
+	date.setHours(date.getHours()+9);
+	return date;
+}
+
+//標準入力からシリアルへ。
 process.stdin.resume();
-process.stdin.setEncoding('utf8');
-process.stdin.on('data', (chunk) => {
-	chunk.trim().split('\n').forEach((line) => {
-		sp.write(line);
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", (chunk) => {
+	chunk.trim().split("\n").forEach((line) => {
+		const cmd = line.split(" ");
+		const task = cmd.shift();
+		const args = cmd.join(" ");
+		beginTasks[task](args);
 	});
 });
