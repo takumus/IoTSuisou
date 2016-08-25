b0e122c7f554fd8000061d5ab45222d010873ce8
takumus <me@takumus.com>
2016-08-25 23:48:04 +0900

    メモリリーク防止

--- src/pi/libs/server.js
+++ src/pi/libs/server.js
@@ -3,11 +3,10 @@ const events = require("events");
 const eventEmitter = new events.EventEmitter();
 const net = require("net");
 const socket = new net.Socket();
-
+let connected = false;
 module.exports = {
 	open:(port, host) => {
-		socket.connect(port, host, ()=>{
-		});
+		socket.connect(port, host);
 	},
 	on:(event, listener) => {
 		eventEmitter.on(event, listener);
@@ -23,11 +22,12 @@ socket.on("data", (data) => {
 	}catch(error){}
 });
 socket.on("connect", () => {
+	connected = true;
 	eventEmitter.emit("open");
 });
 socket.on("error", (error) => {
 	eventEmitter.emit("error");
 });
 socket.on("close", () => {
-	eventEmitter.emit("close");
+	if(connected) eventEmitter.emit("close");
 })
