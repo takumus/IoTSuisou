ffa14d2f3bfacb51b470bc53bddfa50b1e1c6471
takumus <me@takumus.com>
2016-08-27 12:02:23 +0900

    dbへの接続は外でやって、こいつに渡さないとだめだ。

--- src/server/libs/db_waterlevel.js
+++ src/server/libs/db_waterlevel.js
@@ -1,27 +1,25 @@
 "use strict"
 
-const DB = require("./db");
-
-let db;
+let _db;
 let table;
 module.exports = {
-	open:(file, table) => {
-		db = DB.open(file, table);
+	open:(db, table) => {
+		_db = db;
 		init(table);
 		this.table = table;
 	},
 	addData:(value) => {
 		const date = new Date().getTime();
-		db.run("INSERT INTO `" + this.table + "`(`date`, `value`) VALUES(?, ?)", date, value);
+		_db.run("INSERT INTO `" + this.table + "`(`date`, `value`) VALUES(?, ?)", date, value);
 	},
-	getDataEach:(date, count, callback) => {
-		db.each("SELECT `date`, `value` FROM `"+this.table+"` WHERE " + date + " > `date` LIMIT "+count, callback);
+	getData:(date, count, callback) => {
+		_db.each("SELECT `date`, `value` FROM `"+this.table+"` WHERE " + date + " > `date` LIMIT "+count, callback);
 	}
 }
 
 const init = (table) => {
 	//テーブル初期化
-	db.run(
+	_db.run(
 		"CREATE TABLE IF NOT EXISTS `" + table + "` ( \
 			`id`	INTEGER PRIMARY KEY AUTOINCREMENT, \
 			`date`	INTEGER, \
