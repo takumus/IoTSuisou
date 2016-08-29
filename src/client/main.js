f29023db76555f776928f54d02fb0f7b11744f94
takumus <me@takumus.com>
2016-08-29 23:25:45 +0900

    仮だが、餌やり、照明はブラウザからコントロールできる。

--- src/client/main.js
+++ src/client/main.js
@@ -1,23 +1,47 @@
-var send
 window.addEventListener("load",function() {
+	var statusElem = document.getElementById("status");
+	var lightElem = document.getElementById("light");
+	var feedElem = document.getElementById("feed");
 
 	var socket = new WebSocket(CONFIG.host);
 	socket.addEventListener("open", function() {
+		send({method:"status"});
 	});
 	socket.addEventListener("message", function(event) {
-		console.log(JSON.parse(event.data));
+		var data = JSON.parse(event.data);
+		//console.log(data);
+		if(data.method == "status"){
+			receiveStatus(data.status);
+		}
 	});
-	send = function(data) {
+	var send = function(data) {
 		socket.send(JSON.stringify(data));
 	}
 
+	var receiveStatus = function(status){
+		console.log(status);
+		var str = "<b>照明</b>:" + (status.light=="true"?"点灯":"消灯") + "<br>";
+		str += "<b>実行中タスク</b>:" + (status.workingTask?status.workingTask.task:"なし") + "<br>";
+		statusElem.innerHTML = str;
+	}
+
 	//いろいろ
-	document.getElementById("light").addEventListener("change", function(e){
+	lightElem.addEventListener("change", function(e){
 		send({
 			method:"task",
 			task:{
 				task:"light",
 				power:e.target.checked.toString()
+			}
+		});
+	});
+
+	feedElem.addEventListener("click", function(e){
+		send({
+			method:"task",
+			task:{
+				task:"feed",
+				loop:1
 			}
 		});
 	});
