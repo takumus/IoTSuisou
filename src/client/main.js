6a0da06c41d99b9d72ced57e5229a3fb2c0b54e3
takumus <me@takumus.com>
2016-08-30 17:06:03 +0900

    無理やり水量を表示。

--- src/client/main.js
+++ src/client/main.js
@@ -3,11 +3,17 @@ window.addEventListener("load",function() {
 	var statusElem = document.getElementById("status");
 	var lightElem = document.getElementById("light");
 	var feedElem = document.getElementById("feed");
+	var wlElem = document.getElementById("waterlevel");
 
 	var socket = new WebSocket(CONFIG.host);
+
+	var getWaterLevel = function(){
+		send({method:"waterlevel", time:new Date().getTime(), length:100});
+	}
 	socket.addEventListener("open", function() {
 		console.log("connected");
 		send({method:"status"});
+		getWaterLevel();
 	});
 	socket.addEventListener("message", function(event) {
 		var data = JSON.parse(event.data);
@@ -17,6 +23,10 @@ window.addEventListener("load",function() {
 		}
 		if(data.method == "complete"){
 			completeTask(data.result);
+			return;
+		}
+		if(data.method == "waterlevel"){
+			waterlevel(data.datas);
 			return;
 		}
 		console.log("その他データ受信");
@@ -30,12 +40,25 @@ window.addEventListener("load",function() {
 		console.log("ステータス更新");
 		//console.log(status);
 		var str = "<b>照明</b>:" + (status.light=="true"?"点灯":"消灯") + "<br>";
+		lightElem.checked = status.light;
 		str += "<b>実行中タスク</b>:" + (status.workingTask?status.workingTask.task:"なし") + "<br>";
 		statusElem.innerHTML = str;
 	}
 	var completeTask = function(result){
 		console.log("タスク完了");
 		console.log(result);
+	}
+
+	var waterlevel = function(datas){
+		wlElem.innerHTML = "";
+		datas.forEach(function(data){
+			var e = document.createElement("li");
+			var d = new Date(data.date);
+			var v = Math.floor(data.value * 1000)/1000;
+			var dstr = (d.getMonth()+1)+" / "+d.getDate()+" "+d.getHours()+":"+d.getMinutes();
+			e.innerText = dstr + " : " + v + " cm";
+			wlElem.appendChild(e);
+		});
 	}
 
 	//いろいろ
